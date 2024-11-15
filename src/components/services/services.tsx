d9413fc656af22cc14ec84da97a64f2d8e2a5496
Ranim Benkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2024-11-15 12:24:18 +0100

    update

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -4,13 +4,13 @@ import CustomFeature from './custom-feature'
 import stokcge from '../../assets/featured-icon.png'
 export default function Services() {
   const services = [
-    { icon: stokcge, title: 'Stockage', desc: 'Stockez vos marchandises en toute sécurité avec nos installations modernes et surveillées.' },
-    { icon: stokcge, title: 'Conditionnement', desc: 'Emballage professionnel pour garantir la protection de vos colis tout au long du transport.' },
-    { icon: stokcge, title: 'Suivi des colis', desc: 'Restez informé en temps réel de l\'emplacement et du statut de vos colis.' },
-    { icon: stokcge, title: 'Manutention', desc: 'Manipulation sécurisée de vos marchandises pour un traitement sans encombre.' },
-    { icon: stokcge, title: 'Collecte de fond', desc: 'Service sécurisé de collecte pour vos besoins de recouvrement de fonds.' },
-    { icon: stokcge, title: 'Livraison à domicile', desc: 'Recevez vos colis directement chez vous, sans tracas.' }
-]
+    { icon: stokcge, title: 'Livraison à domicile', desc: 'Recevez vos colis directement chez vous, sans tracas.' },
+    { icon: stokcge, title: 'Suivi des colis', desc: 'Restez informés en temps réel de l\'emplacement et du statut de vos colis.' },
+    { icon: stokcge, title: 'Stockage', desc: 'Entreposage sécurisé de vos colis en attente de livraison.' },
+    { icon: stokcge, title: 'Magasinage', desc: 'Stockage sécurisé de vos marchandises en attente de dédouanement, avec des conditions optimales pour préserver leur intégrité.' },
+    { icon: stokcge, title: 'Conventions', desc: 'Mise à disposition d\'une plateforme web dédiée pour nos clients conventionnés, facilitant l\'accès et la gestion de leurs services en ligne.' },
+    { icon: stokcge, title: 'Manutention', desc: 'Manipulation sécurisée de vos marchandises pour un traitement sans encombre.' }
+];
   return (
     <div className='mt-44 flex justify-center flex-col items-center'>
       <ServicesTitle/>
