d35a5dd1927f93da66a6b37c05fbe0d2b211701c
Ranim Benkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2024-11-21 21:28:42 +0100

    up

--- src/components/common/check-robot.tsx
+++ src/components/common/check-robot.tsx
@@ -4,8 +4,10 @@ import InputWithLabel from './input-label'
 
 export default function CheckRobot() {
   return (
-    <div className='max-w-[640px] max-h-[340px] rounded-md shadow-xl bg-white'>
-      <InputWithLabel label='Numero de colis' type='Email' placeHolder='xxxxx-xxxxx'/>
+    <div className='min-w-[640px] min-h-[340px] rounded-md shadow-xl bg-white'>
+        <div className='p-5'>
+      <InputWithLabel label='Numero de colis' type='Email' placeHolder='xxxxx-xxxxx' />
+        </div>
     </div>
   )
 }
--- src/components/suivi/suivi-mon-colis.tsx
+++ src/components/suivi/suivi-mon-colis.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SuiviTitle from './suivi-title'
+import CheckRobot from '../common/check-robot'
 
 export default function SuiviColis() {
   return (
     <div className='flex flex-col justify-center items-center mt-32'>
       <SuiviTitle/>
+      <CheckRobot/>
     </div>
   )
 }
