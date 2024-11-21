2fe24d23d4836b03b45189b68bca6eed11e3730f
Ranim Benkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2024-11-21 21:29:03 +0100

    up

--- src/components/background/bg-image.tsx
+++ src/components/background/bg-image.tsx
@@ -16,6 +16,7 @@ export default function BgImage() {
         left: '50%', // Centering logic
         transform: 'translateX(-50%)', // Ensures proper centering
         zIndex: -1,
+        
         overflow: 'visible',
       }}
     ></div>
