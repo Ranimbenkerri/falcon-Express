e9729e8b18bf5adeb3729d8e5a0b063b827efec9
Ranim Benkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2024-11-22 17:00:15 +0100

    up

--- src/components/contact/contact-content.tsx
+++ src/components/contact/contact-content.tsx
@@ -15,9 +15,7 @@ export default function ContactContent() {
 
 
                     {checkText.map((text, index) => (<CheckedOption key={index} text={text}/>))}
-                </div>
-
-                
+                </div>                
             <SocialMedia/>
         </div>
     </div>
